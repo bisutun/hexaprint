5eff8a144b7ef0cc4bcfb29e90ac53526f8b40c8
bisutun <k.gilhooley@gmail.com>
2014-05-27 23:19:52 -0700

    Update hxp.js

--- hxp.js
+++ hxp.js
@@ -1,14 +1,10 @@
 $(document).ready(function() {
-	$('.button').on('click', function() {
-  $('h4').toggleClass('hide');
-  $('.button2').toggleClass('hide');
-  $(this).toggleClass('hide');
-   $('p').toggleClass('hide');
-});
-	$('.button2').on('click', function () {
-  $('h4').toggleClass('hide');
-    $(this).toggleClass('hide');
-  $('.button').toggleClass('hide');
-  $('p').toggleClass('hide');
-	})
+  function hide() {
+     $('h4').toggleClass('hide');
+     $('.button').toggleClass('hide');
+     $('.button2').toggleClass('hide');
+     $('p').toggleClass('hide');
+  };
+	$('.button').on('click', hide);
+	$('.button2').on('click', hide);
 })	
